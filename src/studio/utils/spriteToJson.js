91769405f3a391885bded102f9ac850c0a602fa7
ulfsaar <ulfsaar0129@outlook.com>
2020-05-13 10:20:54 -0700

    x,y view json is a number not a string

--- src/studio/utils/spriteToJson.js
+++ src/studio/utils/spriteToJson.js
@@ -2,8 +2,10 @@ export const spriteToJsonBubble = (data, width, height) => {
   var formatted = []
   data.forEach(element => {
     const icon = (element.image.substring(0, 4) === 'http') ? element.image : undefined
-    const posX = ((element.x*0.9)/width).toFixed(2)
-    const posY = ((element.y*0.9)/height).toFixed(2)
+    //const posX = ((element.x*0.9)/width)
+    //const posY = ((element.y*0.9)/height).toFixed(2)
+    const posX = Math.round(((element.x*0.9)/width) * 1e2 ) / 1e2;
+    const posY = Math.round(((element.y*0.9)/height) * 1e2 ) / 1e2;
     const obj = {
       title: element.title,
       subcategoryId: element.subcat ? element.subcat : "",
